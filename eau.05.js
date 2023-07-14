c47ecab6c0bf3652d8ec318965015e27d5768c3f
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-14 21:08:25 +0200

    String dans string
    Script qui renvoie true si la seconde chaîne de caractères passée en argument, est contenue dans la première chaîne de caractères passée en argument.
    Accepte les nombres sous forme de chaîne de caractères.

---
+++ eau.05.js
@@ -0,0 +1,74 @@
+/*Fonctions*/
+const compareStrings = (firstString, secondString) => {
+    const firstStringArray = firstString.toLowerCase().split('')
+    const secondStringArray = secondString.toLowerCase().split('')
+    let firstStringIndex = firstSameLetter(firstStringArray, secondStringArray) + 1
+    let result = false
+
+    if (firstStringIndex === null) {
+        return result
+    } else {
+        let compareArray = [
+            secondStringArray[0]
+        ]
+
+        for (i = 1; i < secondStringArray.length; i++) {
+            console.log(i)
+            console.log(firstStringIndex)
+            if (secondStringArray[i] === firstStringArray[firstStringIndex]) {
+                compareArray.push(firstStringArray[i])
+                firstStringIndex += 1
+            } else {
+                return result
+            }
+        }
+
+        if (compareArray.length === secondStringArray.length) {
+            return result = true
+        } else {
+            return result
+        }
+    }
+}
+
+const firstSameLetter = (firstStringArray, secondStringArray) => {
+    let indexFirstLetter = null
+
+    for (i = 0; i < firstStringArray.length; i++) {
+        if (firstStringArray[i] === secondStringArray[0]) {
+            indexFirstLetter = i
+            break
+        }
+    }
+
+    return indexFirstLetter
+}
+
+/*Gestion des erreurs*/
+const isValidArguments = (arguments) => {
+    if (arguments.length === 2) {
+        return arguments
+    } else {
+        return console.log("Une erreur est survenue. Veuillez renseigner au moins un argument.")
+    }
+}
+
+/*Parsing*/
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+/*Résolution*/
+const sayIfHaveSentence = () => {
+    const strings = getArguments()
+
+    if (!isValidArguments(strings)) {
+        return
+    }
+
+    return console.log(compareStrings(strings[0], strings[1]))
+}
+
+/*Affichage du résultat*/
+sayIfHaveSentence()
