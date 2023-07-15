41f74d26518ec5c636e3a68555bb4551d1796954
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-15 19:00:36 +0200

    Index wanted
    Script qui renvoie l'index de l'élément du tableau d'arguments passés ayant la même valeur que le dernier argument du tableau.

---
+++ eau.10.js
@@ -0,0 +1,44 @@
+/*Fonctions*/
+const searchIndex = (arguments) => {
+    let indexArguments = - 1
+    let indexSearched = arguments.length - 1
+
+    for (i = 0; i < indexSearched; i++) {
+        if (arguments[i] === arguments[indexSearched]) {
+            indexSearched = i
+
+            return indexSearched
+        }
+    }
+
+    return indexArguments
+}
+
+/*Gestion des erreurs*/
+const isValidArguments = (arguments) => {
+    if (arguments.length >= 3) {
+        return arguments
+    } else {
+        return console.log("Une erreur est survenue. Veuillez renseigner trois arguments.")
+    }
+}
+
+/*Parsing*/
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+/*Résolution*/
+const getIndexSearched = () => {
+    const arguments = isValidArguments(getArguments())
+
+    if (!arguments) {
+        return
+    }
+
+    return console.log(searchIndex(arguments))
+}
+
+/*Affichage du résultat*/
+getIndexSearched()
