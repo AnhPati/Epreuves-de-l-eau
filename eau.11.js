6ec50c0bd1466b607a377705b80781071d1632c7
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-16 20:16:33 +0200

    Différence minimale absolue
    Script qui affiche l'écart minimale absolu entre les deux nombres les plus proches que contient le tableau d'arguments.

---
+++ eau.11.js
@@ -0,0 +1,104 @@
+/*Fonctions*/
+const gapCalc = (numbers) => {
+    let gapsArray = []
+    let tempNumbers = [0, 0]
+
+    for (i = 0; i < numbers.length; i++) {
+        for (j = 0; j < numbers.length; j++) {
+            if (i !== j) {
+
+                if (numbers[i] >= 0 && numbers[j] >= 0) {
+                    if (numbers[i] >= numbers[j]) {
+                        gapsArray.push(numbers[i] - numbers[j])
+                    } else {
+                        gapsArray.push(Number(numbers[j]) - Number(numbers[i]))
+                    }
+                } else if (numbers[i] < 0 || numbers[j] < 0) {
+                    if (numbers[i] >= numbers[j]) {
+                        tempNumbers[0] = negativeToPositive(numbers[j])
+                        gapsArray.push(Number(numbers[i]) + tempNumbers[0])
+                    } else {
+                        tempNumbers[0] = negativeToPositive(numbers[i])
+                        gapsArray.push(Number(numbers[j]) + tempNumbers[0])
+                    }
+                } else {
+                    if (numbers[i] >= numbers[j]) {
+                        tempNumbers[0] = negativeToPositive(numbers[i])
+                        tempNumbers[1] = negativeToPositive(numbers[j])
+                        gapsArray.push(tempNumbers[1] - tempNumbers[0])
+                    } else {
+                        tempNumbers[0] = negativeToPositive(numbers[i])
+                        tempNumbers[1] = negativeToPositive(numbers[j])
+                        gapsArray.push(tempNumbers[0] - tempNumbers[1])
+                    }
+                }
+            }
+        }
+    }
+
+    console.log("GapArray : " + gapsArray)
+
+    return minGap(gapsArray)
+}
+
+const negativeToPositive = (number) => {
+    console.log(number)
+    let numberString = number.toString().slice(1)
+
+    return Number(numberString)
+}
+
+const minGap = (array) => {
+    let result = array[0]
+
+    for (i = 1; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i]
+        }
+    }
+
+    return result
+}
+
+/*Gestion des erreurs*/
+const isValidArguments = (arguments) => {
+    if (arguments.length >= 2) {
+        return arguments
+    } else {
+        return console.log("Une erreur est survenue. Veuillez renseigner au moins deux arguments.")
+    }
+}
+
+const isValidNumber = (number) => {
+    if (!isNaN(number)) {
+        return Number(number)
+    } else {
+        return console.log("Une erreur est survenue.")
+    }
+}
+
+/*Parsing*/
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+/*Résolution*/
+const getMinGap = () => {
+    let numbers = isValidArguments(getArguments())
+
+    if (!numbers) {
+        return
+    }
+
+    for (i = 0; i < numbers.length; i++) {
+        if (!isValidNumber(numbers[i])) {
+            return
+        }
+    }
+
+    return console.log(gapCalc(numbers))
+}
+
+/*Affichage du résultat*/
+getMinGap()
